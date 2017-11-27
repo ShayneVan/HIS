299544f55af894563f49cb032e3392d5c4bfc249
shayne <shayne@DESKTOP-TVG1JKA>
2017-11-27 16:53:44 +0800

    添加项目文件。

---
+++ HIS/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HIS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        public ActionResult guahao()
+        {
+            return View();
+        }
+        public ActionResult guahao_next()
+        {
+            return View();
+        }
+        public ActionResult guahaoByD()
+        {
+            return View();
+        }
+    }
+}
